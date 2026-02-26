0679826561ce5d61b700257fa2f6e4b3dcd779fb
lottie <lottiekxiv@proton.me>
2026-02-25 18:08:39 -0600

    yippeeeee

---
+++ home.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+{
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "lottie";
+  home.homeDirectory = "/home/lottie";
+  
+  programs.zsh = {
+  enable = true;
+
+    # With Zplug:
+    zplug = {
+      enable = true;
+      plugins = [
+        {name = "zsh-users/zsh-autosuggestions";} # Simple plugin installation
+      ];
+    };
+
+    # With Oh-My-Zsh:
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"         # also requires `programs.git.enable = true;`
+    ];
+      theme = "robbyrussell";
+    };
+  };
+ 
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  programs.git = {
+    enable = true;
+    userEmail = "lottiekxiv@proton.me";
+    # userName = "lottie";
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/lottie/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
