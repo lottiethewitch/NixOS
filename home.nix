95fcbee8fa9c5d4606831d58dbe87a73e1118216
lottie <lottiekxiv@proton.me>
2026-02-25 19:46:08 -0600

    yyaoa

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 {
-
+  imports =  [
+   nvim.nix
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "lottie";
