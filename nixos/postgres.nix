dca73eee372a25130ba2fd2529c83d3617f99ea1
lottie <lottiekxiv@proton.me>
2026-03-08 12:28:08 -0500

    pass at understanding flakes

--- nixos/postgres.nix
+++ nixos/postgres.nix
@@ -4,7 +4,7 @@
   config.services.postgresql = {
     enable = true; 
     ensureDatabases = [ "vaderbase" ];
-    authentication = pkgs.lib.mkOverrride 10 ''
+    authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
     '';
