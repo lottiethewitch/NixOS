0d3e62cc2f4b962326918630a6d9fbabc855d87f
lottie <lottiekxiv@proton.me>
2026-03-18 19:53:31 -0500

    modules ref

---
+++ nixscripts/gitcom.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash
+
+MSG = "Automated git commit message at %(%Y-%m-%d %H:%M:%S)T\n"
+
+git add -A
+git commit $MSG
+
+while getopts "h:s:" opt; do
+  case $opt in
+	h) home-manager switch --flake .#lottie@vader --show-trace
+	s) sudo nixos-rebuild switch --flake .#vader
+	\?) echo "Invalid option: -$OPTARG" ;;
+  esac
+done
+
+
