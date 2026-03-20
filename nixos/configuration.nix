8395510ed75b318ae9755735bde12d711318b8cf
lottie <lottiekxiv@proton.me>
2026-03-20 18:59:04 -0500

    Automated git commit message at 20-03-2026 18:59:04

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -45,7 +45,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   # Virtualization
 
-  virtualization.libvirtd = {
+  virtualisation.libvirtd = {
 	enable = true;
 	qemu = {
 		package = pkgs.qemu_kvm;
