44de1d82483096171913c4e1173b51fbb6c97a82
lottie <lottiekxiv@proton.me>
2026-03-20 22:23:35 -0500

    Automated git commit message at 20-03-2026 22:23:35

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,6 +29,12 @@
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
+    
+    gc = {
+	enabled = true;
+	automatic = true;
+	options = "--delete-older-than 30d";
+    };
     # Opinionated: disable channels
     channel.enable = false;
 
