65727308afdc1e53cac3d3cec7761f5305dd6341
lottie <lottiekxiv@proton.me>
2026-05-13 16:26:35 -0500

    Automated git commit message at 13-05-2026 16:26:35

--- nixos/vader/vader.nix
+++ nixos/vader/vader.nix
@@ -41,6 +41,7 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   # Virtualization
+  services.rpcbind.enable = true;
 
   virtualisation = {
      libvirtd = {
