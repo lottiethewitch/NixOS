e6b74d045b29d75cb7a5845846166ecb4beb3ff1
Your Name <you@example.com>
2026-06-21 14:23:25 -0500

    Automated git commit message at 21-06-2026 14:23:25

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,10 +1,5 @@
-{ pkgs, ... }: 
+{  pkgs, ... }: 
 {
-	programs.steam = {
-		enable = true;
-		 localNetworkGameTransfers.openFirewall = true;    
-	};
-
 	environment.systemPackages = with pkgs; [
 		steam-run
 	];
