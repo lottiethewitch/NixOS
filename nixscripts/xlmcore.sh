a052677245aeedc18e7f3039fdb73a0fcab87c35
Your Name <you@example.com>
2026-06-22 17:53:14 -0500

    Automated git commit message at 22-06-2026 17:53:14

--- nixscripts/xlmcore.sh
+++ nixscripts/xlmcore.sh
@@ -3,23 +3,25 @@
 
 #!/bin/sh -e
 
-echo "-- XLM Native Auto-Installer --"
-echo
 
-if [ "$(id -u)" -eq 0 ]; then
-    echo 'This script cannot be ran as the root user or with sudo. Please run it as a regular user.'
-    exit 1
-fi
+echo "-- XLM Native Auto-Installer --"
+echo ""
 
 echo "[Step: 1] Downloading XLM"
-curl --fail -L https://github.com/Blooym/xlm/releases/latest/download/xlm-x86_64-unknown-linux-gnu > /home/lottie/xlm
+curl -L https://github.com/Blooym/xlm/releases/latest/download/xlm-x86_64-unknown-linux-gnu > /tmp/xlm
 
-echo "[Step: 2] Configuring XLM as a Steam Tool"
-chmod +x /home/lottie/xlm
-/home/lottie/xlm install-steam-tool --xlm-updater-disable --extra-launch-args="--run-as-steam-compat-tool=true" --steam-compat-path ~/.steam/root/compatibilitytools.d/
+echo "[Step: 2] Configuring XLM as a Steam Tool using XIVLauncher-RB"
+chmod +x /tmp/xlm
+mkdir -p /tmp/xlm-rb
+/tmp/xlm install-steam-tool --extra-launch-args="--xlcore-repo-owner rankynbass" --steam-compat-path /tmp/xlm-rb
+sed -i 's/XLCore/XLCore-RB/' /tmp/xlm-rb/XLM/compatibilitytool.vdf
+sed -i 's/"xlm"/"xlm-rb"/' /tmp/xlm-rb/XLM/compatibilitytool.vdf
+sed -i 's/"xlm"/"xlm-rb"/' /tmp/xlm-rb/XLM/toolmanifest.vdf
+mkdir -p ~/.steam/root/compatibilitytools.d/XLM-RB
+mv /tmp/xlm-rb/XLM/* ~/.steam/root/compatibilitytools.d/XLM-RB/
 
 echo "[Step: 3] Cleanup XLM binary"
-rm /home/lottie/xlm
+rm /tmp/xlm
 
-echo
-echo "-- Auto Installer Complete: Restart Steam and follow the guide at https://goatcorp.github.io/faq/steamdeck or https://github.com/Blooym/xlm#readme to continue! --"
+echo ""
+echo "-- Auto Installer Complete: Restart Steam and follow the README to continue! --"
