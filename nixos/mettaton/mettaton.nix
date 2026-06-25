6355606de9bb6052fa6982550278e721e315b801
lottie <lottiekxiv@proton.me>
2026-06-25 17:20:13 -0500

    Automated git commit message at 25-06-2026 17:20:13

--- nixos/mettaton/mettaton.nix
+++ nixos/mettaton/mettaton.nix
@@ -80,6 +80,9 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+
+  services.mullvad-vpn.enable = true;
+
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
