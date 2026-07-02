f604664bbd388ffd1c8a7cb5ab595ab18134a2a9
Your Name <you@example.com>
2026-07-02 13:57:37 -0500

    Automated git commit message at 02-07-2026 13:57:37

--- nixos/modules/ui/gnome.nix
+++ nixos/modules/ui/gnome.nix
@@ -1,11 +1,40 @@
-{ config, pkgs, ... }: 
+{ pkgs, ... }: 
 
 {
-   services.displayManager.gdm.enable = true; 
-   services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true; 
+  services.desktopManager.gnome.enable = true;
 
-   services.gnome.core-apps.enable = true; 
-   services.gnome.core-developer-tools.enable = true;
-   services.gnome.games.enable = false; 
-   environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs];
+  services.gnome.core-apps.enable = false; 
+  services.gnome.core-developer-tools.enable = true;
+  services.gnome.games.enable = false; 
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs];
+
+  environment.systemPackages = with pkgs; [
+	xdg-desktop-portal-gnome
+	xdg-desktop-portal-gtk
+	gnomeExtensions.dash-to-panel
+	gnome-extension-manager
+  ];
+
+  xdg = {
+	portal = {
+	  enable = true;
+	  wlr = {
+		enable = true;
+		settings = {
+		  screencast = {
+			chooser_type = "simple";
+			chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
+		  };
+		};
+	  };
+	  config = {
+		common.default = [ "kde" ];
+	  };
+	  extraPortals = [
+		pkgs.xdg-desktop-portal-gtk
+		pkgs.kdePackages.xdg-desktop-portal-kde
+	  ];
+	};
+  };
 }
