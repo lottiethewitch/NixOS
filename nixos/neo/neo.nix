a95008c272589fc5c930a2f58ddc2b30c2960003
Your Name <you@example.com>
2026-08-15 17:18:57 -0500

    Automated git commit message at 15-08-2026 17:18:57

--- nixos/neo/neo.nix
+++ nixos/neo/neo.nix
@@ -175,6 +175,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+	brave
 	terminator
 	calibre
 	libimobiledevice
