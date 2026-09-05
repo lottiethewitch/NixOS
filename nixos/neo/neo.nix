6712637be147c6109a9596211ebfcd062989f926
lottie <lottiekxiv@proton.me>
2026-09-05 13:29:28 -0500

    Automated git commit message at 05-09-2026 13:29:28

--- nixos/neo/neo.nix
+++ nixos/neo/neo.nix
@@ -166,12 +166,7 @@
 	allowUnfree = true;
 	android_sdk.accept_license = true;
   };  
-
-  programs.steam = {
-	enable = true;
-	localNetworkGameTransfers.openFirewall = true;    
-  };
-
+  
   services.flatpak.enable = true;
 
   services.usbmuxd = {
