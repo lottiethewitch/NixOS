d970785a0f7e1e1f04dfd9e73a9620cbf5ae6b59
Your Name <you@example.com>
2026-09-14 14:21:26 -0500

    Automated git commit message at 14-09-2026 14:21:26

--- nixos/modules/ui/niri.nix
+++ nixos/modules/ui/niri.nix
@@ -1,18 +1,7 @@
 { pkgs, ... }:
 
 {
-
   programs.niri.enable = true;
-
-  services.greetd = {
-	enable = true;
-	settings = {
-	  default_session = {
-		command = "${config.programs.niri.package}/bin/niri-session";
-		user = "lottie";
-	  };
-	};
-  };
 
   environment.systemPackages = with pkgs; [
 	alacritty
