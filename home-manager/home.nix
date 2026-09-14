96209c17ee29cd4848642db0aaed4a7342bfdde2
Your Name <you@example.com>
2026-09-14 14:30:18 -0500

    Automated git commit message at 14-09-2026 14:30:18

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -34,6 +34,9 @@
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
+
+  xdg.configFile."niri/config.kdl".source = ./dotfiles/niri/config.kdl;
+
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
