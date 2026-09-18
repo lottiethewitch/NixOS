8cc61513d2c741286f7e66c4b4e30ebc5e7fbcaa
Your Name <you@example.com>
2026-09-17 20:28:00 -0500

    Automated git commit message at 17-09-2026 20:28:00

--- nixos/modules/display/default.nix
+++ nixos/modules/display/default.nix
@@ -1,17 +1,16 @@
 { pkgs, ... }:
 
-{
-  # This module is just kind of going to manage both my display manager and xdg because I want to unify them and cannot think of a better place to put them tbh!!
-  theme = let
-	sddm-astronaut = (pkgs.sddm-astronaut.override {
-	  embeddedTheme = "japanese_aesthetic";  # or any other theme
-	  themeConfig = {
-		# Customize colors and settings
-		HeaderTextColor = "#d5c4a1";
-		Background = "../../sysui/cyberpunk-city-night-view-4k-8l.jpg";
-		# ... other theme configuration options
-	  };
-	}).overrideAttrs (oldAttrs: {
+# This module is just kind of going to manage both my display manager and xdg because I want to unify them and cannot think of a better place to put them tbh!!
+let
+  sddm-astronaut = (pkgs.sddm-astronaut.override {
+	embeddedTheme = "japanese_aesthetic";  # or any other theme
+	themeConfig = {
+	  # Customize colors and settings
+	  HeaderTextColor = "#d5c4a1";
+	  Background = "../../sysui/cyberpunk-city-night-view-4k-8l.jpg";
+	  # ... other theme configuration options
+	};
+  }).overrideAttrs (oldAttrs: {
 	  # Optional: Inject custom background image
 	  installPhase = oldAttrs.installPhase + ''
 		chmod u+w $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
@@ -19,24 +18,23 @@
 		$out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/cyberpunk-city-night-view-4k-8l.jpg
 	  '';
 	});
-  in {
-	environment.systemPackages = [ sddm-astronaut ];
+in {
 
-	services.displayManager.sddm = {
-	  enable = true;
-	  package = pkgs.kdePackages.sddm;
-	  extraPackages = with pkgs; [
-		kdePackages.qtmultimedia # Required for video backgrounds/audio
-	  ];
-	  theme = "sddm-astronaut-theme";
-	};
+  services.displayManager.sddm = {
+	enable = true;
+	package = pkgs.kdePackages.sddm;
+	extraPackages = with pkgs; [
+	  kdePackages.qtmultimedia # Required for video backgrounds/audio
+	];
+	theme = "sddm-astronaut-theme";
   };
+
   environment.systemPackages = with pkgs; [
+	sddm-astronaut
 	xdg-desktop-portal-gtk
 	xdg-desktop-portal-gnome
   ];
 
-  services.displayManager.sddm.enable = true;
 
   xdg = {
 	portal = {
@@ -44,15 +42,15 @@
 	  wlr = {
 		enable = true;
 		settings = {
-		 screencast = {
-		  chooser_type = "simple";
-		  chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
+		  screencast = {
+			chooser_type = "simple";
+			chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
+		  };
 		};
 	  };
-	};
-	config = {
-	  common.default = [  "gtk" "kde" ];
-	};
+	  config = {
+		common.default = [  "gtk" "kde" ];
+	  };
 	  extraPortals = [
 		pkgs.xdg-desktop-portal-gtk
 		pkgs.xdg-desktop-portal-gnome
