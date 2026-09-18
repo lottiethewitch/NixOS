d51017fae61c8fa34ce5c89bde5733cc32dd650e
Your Name <you@example.com>
2026-09-17 20:45:19 -0500

    Automated git commit message at 17-09-2026 20:45:19

--- nixos/neo/neo.nix
+++ nixos/neo/neo.nix
@@ -1,5 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { inputs, config, pkgs, lib, ... }:
@@ -185,7 +183,6 @@
 	mtools
 	nfs-utils
 	adwaita-icon-theme
-	htop
 	xivlauncher
 	wine-staging
 	xclip
