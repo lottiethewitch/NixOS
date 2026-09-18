52c60da0de11f07f1ff5221fb39a802fdb11c1a5
Your Name <you@example.com>
2026-09-17 20:18:58 -0500

    Automated git commit message at 17-09-2026 20:18:58

---
+++ nixos/modules/display/default.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }:
+
+{
+  # This module is just kind of going to manage both my display manager and xdg because I want to unify them and cannot think of a better place to put them tbh!!
+  let
+	sddm-astronaut = (pkgs.sddm-astronaut.override {
+	  embeddedTheme = "japanese_aesthetic";  # or any other theme
+	  themeConfig = {
+		# Customize colors and settings
+		HeaderTextColor = "#d5c4a1";
+		Background = "Backgrounds/your-custom-background.png";
+		# ... other theme configuration options
+	  };
+	}).overrideAttrs (oldAttrs: {
+	  # Optional: Inject custom background image
+	  installPhase = oldAttrs.installPhase + ''
+		chmod u+w $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
+		cp ${./relative/path/to/your-custom-background.png} \
+		$out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/your-custom-background.png
+	  '';
+	});
+  in {
+	environment.systemPackages = [ sddm-astronaut ];
+
+	services.displayManager.sddm = {
+	  enable = true;
+	  package = pkgs.kdePackages.sddm;
+	  extraPackages = with pkgs; [
+		kdePackages.qtmultimedia # Required for video backgrounds/audio
+	  ];
+	  theme = "sddm-astronaut-theme";
+	};
+  }
+  environment.systemPackages = with pkgs; [
+	xdg-desktop-portal-gtk
+	xdg-desktop-portal-gnome
+  ];
+
+  services.displayManager.sddm.enable = true;
+
+  xdg = {
+	portal = {
+	  enable = true;
+	  wlr = {
+		enable = true;
+		settings = {
+		 screencast = {
+		  chooser_type = "simple";
+		  chooser_cmd = "${pkgs.slurp}/bin/slurp -f 'Monitor: %o' -or";
+		};
+	  };
+	};
+	config = {
+	  common.default = [  "gtk" "kde" ];
+	};
+	  extraPortals = [
+		pkgs.xdg-desktop-portal-gtk
+		pkgs.xdg-desktop-portal-gnome
+	  ];
+	};
+  };
+}
